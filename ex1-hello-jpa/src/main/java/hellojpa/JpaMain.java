40dd5bf0cfe54a119ea880b005a1ff383ccefa72
GGHDMS <tjrans303@naver.com>
2022-09-27 21:27:31 +0900

    값 타입 컬렉션 까지

--- ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -7,6 +7,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.security.Principal;
 import java.util.List;
 
 
@@ -19,13 +20,33 @@ public class JpaMain {
         tx.begin();
 
         try{
-
             Member member = new Member();
-            member.setUsername("hello");
-            member.setHomeAddress(new Address("city", "street", "zipcode"));
-            member.setWorkPeriod(new Period());
+            member.setUsername("member1");
+            member.setHomeAddress(new Address("city1", "street", "10000"));
+
+            member.getFavoriteFoods().add("치킨");
+            member.getFavoriteFoods().add("족발");
+            member.getFavoriteFoods().add("피자");
+
+            member.getAddressesHistory().add(new Address("old1", "street", "10000"));
+            member.getAddressesHistory().add(new Address("old12", "street", "10000"));
 
             em.persist(member);
+
+            em.flush();
+            em.clear();
+
+            Member findMember = em.find(Member.class, member.getId());
+            Address address = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("newCity", address.getStreet(), address.getZipcode()));
+
+            //치킨 -> 한신
+            findMember.getFavoriteFoods().remove("치킨");
+            findMember.getFavoriteFoods().add("한신");
+
+            findMember.getAddressesHistory().remove(new Address("old1", "street", "10000"));
+            findMember.getAddressesHistory().add(new Address("newCity1", "street", "10000"));
+
             tx.commit();
         } catch (Exception e){
             tx.rollback();
--- ex1-hello-jpa/src/main/java/hellojpa/Member.java
+++ ex1-hello-jpa/src/main/java/hellojpa/Member.java
@@ -2,6 +2,10 @@ package hellojpa;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 
 @Entity
 public class Member {
@@ -20,8 +24,34 @@ public class Member {
     @Embedded
     private Address homeAddress;
 
+    @ElementCollection
+    @CollectionTable(name = "FAVORITE_FOOD", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    @Column(name = "FOOD_NAME")
+    private Set<String> favoriteFoods = new HashSet<>();
+
+    @ElementCollection
+    @CollectionTable(name = "ADDRESS", joinColumns =
+        @JoinColumn(name = "MEMBER_ID")
+    )
+    private List<Address> addressesHistory = new ArrayList<>();
+
+
     public Long getId() {
         return id;
+    }
+
+    public Set<String> getFavoriteFoods() {
+        return favoriteFoods;
+    }
+
+    public void setFavoriteFoods(Set<String> favoriteFoods) {
+        this.favoriteFoods = favoriteFoods;
+    }
+
+    public List<Address> getAddressesHistory() {
+        return addressesHistory;
     }
 
     public void setId(Long id) {
