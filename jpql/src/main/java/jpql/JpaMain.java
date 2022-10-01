1284a07f89c48a7acb60ebb2b3b7bb575d8e3292
허석문 <tjrans303@soongsil.ac.kr>
2022-10-01 21:06:16 +0900

    프로젝션(SELECT)

--- jpql/src/main/java/jpql/JpaMain.java
+++ jpql/src/main/java/jpql/JpaMain.java
@@ -4,6 +4,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 
 public class JpaMain {
@@ -17,7 +18,16 @@ public class JpaMain {
         try{
             Member member = new Member();
             member.setUsername("member1");
+            member.setAge(10);
             em.persist(member);
+
+            List<MemberDTO> result = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+
+            MemberDTO memberDTO = result.get(0);
+            System.out.println("memberDTO " + memberDTO.getUsername());
+            System.out.println("memberDTO " + memberDTO.getAge());
+
 
             tx.commit();
         } catch (Exception e){
