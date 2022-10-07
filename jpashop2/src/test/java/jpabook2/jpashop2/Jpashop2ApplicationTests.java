f1b837d32b418cd8fa2d41c68792c8df454a1855
허석문 <tjrans303@soongsil.ac.kr>
2022-10-07 20:56:13 +0900

    프로젝트 생성

---
+++ jpashop2/src/test/java/jpabook2/jpashop2/Jpashop2ApplicationTests.java
@@ -0,0 +1,16 @@
+package jpabook2.jpashop2;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+class Jpashop2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
