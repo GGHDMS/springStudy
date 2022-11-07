fef63c3bc66c3b86863e33448daa0aa77e7b5e7f
허석문 <tjrans303@soongsil.ac.kr>
2022-11-07 19:57:55 +0900

    상품 서비스 개발

---
+++ jpashop2/src/main/java/jpabook2/jpashop2/service/ItemService.java
@@ -0,0 +1,29 @@
+package jpabook2.jpashop2.service;
+
+import jpabook2.jpashop2.domain.item.Item;
+import jpabook2.jpashop2.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+@Transactional(readOnly = true)
+public class ItemService {
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item){
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItem(){
+        return itemRepository.findAll();
+    }
+
+    public Item findOne(Long itemId){
+        return itemRepository.findOne(itemId);
+    }
+}
