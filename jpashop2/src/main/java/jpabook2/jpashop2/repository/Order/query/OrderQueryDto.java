c0f10d66025eb2946e25cf4f700734391e0fd407
허석문 <tjrans303@soongsil.ac.kr>
2022-12-27 15:12:38 +0900

    api 개발 고급 정리

--- jpashop2/src/main/java/jpabook2/jpashop2/repository/Order/query/OrderQueryDto.java
+++ jpashop2/src/main/java/jpabook2/jpashop2/repository/Order/query/OrderQueryDto.java
@@ -3,11 +3,13 @@ package jpabook2.jpashop2.repository.Order.query;
 import jpabook2.jpashop2.domain.Address;
 import jpabook2.jpashop2.domain.OrderStatus;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 import java.util.List;
 
 @Data
+@EqualsAndHashCode(of = "orderId") //groupby 할때 뭐로 묶을건지
 public class OrderQueryDto {
 
     private Long orderId;
@@ -17,11 +19,21 @@ public class OrderQueryDto {
     private Address address;
     private List<OrderItemQueryDto> orderItems;
 
+
     public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address) {
         this.orderId = orderId;
         this.name = name;
         this.orderDate = orderDate;
         this.orderStatus = orderStatus;
         this.address = address;
+    }
+
+    public OrderQueryDto(Long orderId, String name, LocalDateTime orderDate, OrderStatus orderStatus, Address address, List<OrderItemQueryDto> value) {
+        this.orderId = orderId;
+        this.name = name;
+        this.orderDate = orderDate;
+        this.orderStatus = orderStatus;
+        this.address = address;
+        this.orderItems = value;
     }
 }
