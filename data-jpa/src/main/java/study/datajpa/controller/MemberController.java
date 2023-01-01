059a0273beb96e29d103329590d49343c1d324fe
허석문 <tjrans303@soongsil.ac.kr>
2023-01-01 22:28:11 +0900

    도메인 클래스 컨버터

---
+++ data-jpa/src/main/java/study/datajpa/controller/MemberController.java
@@ -0,0 +1,32 @@
+package study.datajpa.controller;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import study.datajpa.entity.Member;
+import study.datajpa.repository.MemberRepository;
+
+import javax.annotation.PostConstruct;
+
+@RestController
+@RequiredArgsConstructor
+public class MemberController {
+    private final MemberRepository memberRepository;
+
+    @GetMapping("/members/{id}")
+    public String findMember(@PathVariable("id") Long id){
+        Member member = memberRepository.findById(id).get();
+        return member.getUsername();
+    }
+
+    @GetMapping("/members2/{id}")
+    public String findMember2(@PathVariable("id") Member member){ //domain class 컨버터 // 권장 x 사용시 조회용으로만 사용
+        return member.getUsername();
+    }
+
+    @PostConstruct
+    public void init(){
+        memberRepository.save(new Member("userA"));
+    }
+}
