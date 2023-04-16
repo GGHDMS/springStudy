86be4609796d11734c215b4964cee1db78e60837
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-04-16 15:33:22 +0900

    JDBC 개발 - 등록

---
+++ jdbc/src/main/java/hello/jdbc/repostiory/MemberRepositoryV0.java
@@ -0,0 +1,67 @@
+package hello.jdbc.repostiory;
+
+import hello.jdbc.connection.DBConnectionUtil;
+import hello.jdbc.domain.Member;
+import lombok.extern.slf4j.Slf4j;
+
+import java.sql.*;
+
+/**
+ * JDBC - DriverManager 사용
+ */
+
+@Slf4j
+public class MemberRepositoryV0 {
+
+    public Member save(Member member) throws SQLException {
+        String sql = "insert into member(member_id, money) values (? , ?)";
+
+        Connection con = null;
+        PreparedStatement pstmt = null; // SQL Injection 공격을 예방하기 위해 무조건 사용, 사용안하면 sql 구문이 대입 될 수도 있다
+
+        con = getConnection();
+        try {
+            pstmt = con.prepareStatement(sql);
+            pstmt.setString(1, member.getMemberId());
+            pstmt.setInt(2, member.getMoney());
+            pstmt.executeUpdate();
+            return member;
+        } catch (SQLException e) {
+            log.error("db error", e);
+            throw e;
+        } finally {
+            close(con, pstmt, null);
+        }
+    }
+
+    private void close(Connection con, Statement stmt, ResultSet rs) { // 외부 자원을 사용하는 것 이기 때문에 꼭 close 해야 한다.
+
+        if (rs != null) {
+            try {
+                rs.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (stmt != null) {
+            try {
+                stmt.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+
+        if (con != null) {
+            try {
+                con.close();
+            } catch (SQLException e) {
+                log.info("error", e);
+            }
+        }
+    }
+
+    private Connection getConnection() {
+        return DBConnectionUtil.getConnection();
+    }
+}
