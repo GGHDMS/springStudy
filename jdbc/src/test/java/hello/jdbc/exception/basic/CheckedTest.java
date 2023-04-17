770b3dc67ce33fbb6659cfab4d9b8c3ae9765d2f
HeoSeokMun <tjrans303@soongsil.ac.kr>
2023-04-18 08:42:13 +0900

    체크 예외 기본 이해

---
+++ jdbc/src/test/java/hello/jdbc/exception/basic/CheckedTest.java
@@ -0,0 +1,67 @@
+package hello.jdbc.exception.basic;
+
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
+@Slf4j
+public class CheckedTest {
+
+    @Test
+    void checked_catch() {
+        Service service = new Service();
+        service.call();
+    }
+
+    @Test
+    void checked_throw() {
+        Service service = new Service();
+        assertThatThrownBy(service::callThrow)
+                .isInstanceOf(MyCheckedException.class);
+    }
+
+
+    static class MyCheckedException extends Exception {
+        public MyCheckedException(String message) {
+            super(message);
+        }
+    }
+
+    /**
+     * Checked 예외는
+     * 예외를 잡아서 처리하거나, 던지거나 둘중 하나를 필수로 선택해야 한다.
+     */
+    static class Service {
+        Repository repository = new Repository();
+
+        /**
+         * 예외를 잡아 처리하는 로직
+         */
+        public void call() {
+            try {
+                repository.call();
+            } catch (MyCheckedException e) {
+                // 예외 처리 로직
+                log.info("예외 처리, message={}", e.getMessage(), e);
+            }
+        }
+
+        /**
+         * 체크 예외를 밖으로 던지는 코드
+         * 체크 예외는 예외를 잡지 않고 밖으로 던지려면 throws 메서드를 필수로 선언해댜 된다.
+         */
+        public void callThrow() throws MyCheckedException {
+            repository.call();
+        }
+    }
+
+    static class Repository {
+        public void call() throws MyCheckedException {
+            throw new MyCheckedException("ex");
+        }
+    }
+
+
+
+}
